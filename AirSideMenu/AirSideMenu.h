33c04fb077258f2887d78c0659adc6d52a9578f1
kirthi <Kirthi.shalom@gmail.com>
2018-10-23 10:51:35 +0530

    Carthage

---
+++ AirSideMenu/AirSideMenu.h
@@ -0,0 +1,19 @@
+//
+//  AirSideMenu.h
+//  AirSideMenu
+//
+//  Created by kirthi on 23/10/18.
+//  Copyright © 2018 kirthi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AirSideMenu.
+FOUNDATION_EXPORT double AirSideMenuVersionNumber;
+
+//! Project version string for AirSideMenu.
+FOUNDATION_EXPORT const unsigned char AirSideMenuVersionString[];
+
+#import <ITRAirSideMenu/ITRAirSideMenu.h>
+
+
